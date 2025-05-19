30f18f29788b05a62723de1fc98b37a6dd1f9435
Ionut Banu <ionutbanu094@gmail.com>
2025-05-19 22:07:53 +0300

    Refactor main.py to use stdio client for session management
    - Replaced MultiServerMCPClient with stdio_client for improved session handling.
    - Introduced ClientSession and StdioServerParameters for better integration with the math server.
    - Updated agent invocation to utilize loaded MCP tools and print results from math queries.

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import os
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_mcp_adapters.tools import load_mcp_tools
 from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from mcp.server.fastmcp import FastMCP
 
 load_dotenv()
 
@@ -18,23 +21,28 @@ llm = ChatGoogleGenerativeAI(
     max_retries=2,
 )
 
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=[
+        "/Users/ionutbanu/Documents/Personal/Workspace/mcp-crash-course/servers/math_server.py"
+    ],
+)
+
 
 async def main():
-    async with MultiServerMCPClient(
-        {
-            "math": {
-                "command": "python",
-                "args": [
-                    "/Users/ionutbanu/Documents/Personal/Workspace/mcp-crash-course/servers/math_server.py"
-                ],
-            }
-        }
-    ) as client:
-        agent = create_react_agent(llm, client.get_tools())
-        result = await agent.ainvoke(
-            {"messages": [HumanMessage(content="What is 54 + 2 * 3?")]}
-        )
-        print(result["messages"][-1].content)
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("Session initialized")
+            tools = await load_mcp_tools(session)
+            print(tools)
+
+            agent = create_react_agent(llm, tools)
+
+            result = await agent.ainvoke(
+                {"messages": [HumanMessage(content="What is 54 + 2 * 3?")]}
+            )
+            print(result["messages"][-1].content)
 
 
 if __name__ == "__main__":
