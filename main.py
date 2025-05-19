0e129c324d347cb6e03b0b268a0715ede769774f
Ionut Banu <ionutbanu094@gmail.com>
2025-05-19 14:29:55 +0300

    Add Google API integration and update dependencies
    - Introduced .env file to store the Google API key.
    - Enhanced main.py to initialize the ChatGoogleGenerativeAI model for Google API interactions.
    - Updated pyproject.toml to include langchain-google-genai and isort as dependencies.
    - Adjusted uv.lock to reflect new dependencies and their versions.
    - Minor formatting improvements in weather_server.py for consistency.

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+import asyncio
+import os
+
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from mcp.server.fastmcp import FastMCP
+
+load_dotenv()
+
+# Initialize model
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/ionutbanu/Documents/Personal/Workspace/mcp-crash-course/servers/math_server.py"],
+)
+
 def main():
     print("Hello from mcp-crash-course!")
 
